dac773c926b4a28933ddffbc81b37d4f3a358c00
mashfied <victoreslieh@gmail.com>
2024-09-22 22:31:46 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {  
+        console.log(i);  
+        i--;  
+    }
+    return i;
+}
+function writeCards(names, eventName) {
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
